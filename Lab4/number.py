7d75694b2e6b6dbc2c327992ada87824831a32ea
Lubna Salhab <lubna13@meet.mit.edu>
2014-02-05 20:00:15 +0200

    lab4

---
+++ Lab4/number.py
@@ -0,0 +1,17 @@
+class Integer(object):
+	def __init__(self, number, posneg):
+		self.number = number
+		self.posneg = posneg
+	def display(self):
+		x = int(self.number)
+		print x 
+if __name__=="__main__":
+	print "Lubna"
+	test = Integer(16)
+	test.display()
+	
+	
+
+
+
+		
